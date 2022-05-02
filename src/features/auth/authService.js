26b655e05ec941d2c0db7962925cbc53da155306
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2022-05-03 00:05:45 +0800

    jwt_decode

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,15 +1,18 @@
+
 import axios from "axios";
 import { API } from './authPath'
+// import jwt_decode from 'jwt-decode'
 
 const register = async (userData) => {
     const response = await axios.post(API+'/register', userData)
-    if (response.data) localStorage.setItem('user', JSON.stringify(response.data))
+    if (response.data) localStorage.setItem('user', JSON.stringify(response.data.token))
     return response.data
 }
 
 const login = async (userData) => {
     const response = await axios.post(API+'/login', userData)
-    if (response.data) localStorage.setItem('user', JSON.stringify(response.data))
+    if (response.data) localStorage.setItem('user', JSON.stringify(response.data.token))
+    // console.log(jwt_decode(response.data.token)) //very important
     return response.data
 }
 
