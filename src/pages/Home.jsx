765fe9ba2c8af03ab3c0d19cc576955a86fe6de7
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2022-05-20 10:08:26 +0800

    Added user credentials insturctions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,11 @@ import Card from '../components/Card'
 const Home = () => {
   return (
     <Card title='Welcome to RRMU'>
-      <p>A very simple React App with Redux-Toolkit with authentication with multiple user roles</p>
+      <p>A very simple React App with Redux-Toolkit with authentication with multiple user roles.</p>
+      <p>User roles:</p>
       <ul>
-        <li>User roles:</li>
-        <ul>
-          <li>Admin</li>
-          <li>subscriber</li>
-        </ul>
+          <li>Admin: admin@gmail.com | admin</li>
+          <li>Subscriber: test@gmail.com | test1234</li>
       </ul>
     </Card>
   )
